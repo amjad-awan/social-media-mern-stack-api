81155f47adce7c89c52a3f8d3b1ee3154594b038
usamaashraf127 <usamaashraf127@gmail.com>
2025-10-19 17:09:15 +0500

    public folder added

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-
 const connectDB= async()=>{
   try{
   const conn= await mongoose.connect(process.env.MONGO_DB);
